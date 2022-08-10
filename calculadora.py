a6da15c68ba5b665ac82936b1f3d699539639288
beatriz <biangomes0707@gmail.com>
2022-08-09 19:55:38 -0500

    implementar a divisao entre dois numeros de forma errada propositalmente (#2)

--- calculadora.py
+++ calculadora.py
@@ -9,6 +9,7 @@ print(f'{x} - {y} = {x - y}')
 
 # Multiplicacao
 <<<<<<< HEAD
+<<<<<<< HEAD
 print(f'{x} x {y} = {x * y}')
 
 # Divisao
@@ -16,3 +17,9 @@ print(f'{x} / {y} = {x + y}')
 =======
 print(f'{x} x {y} = {x * y}')
 >>>>>>> b9cb0e0 (implementar a multiplicacao entre dois numeros (#2))
+=======
+print(f'{x} x {y} = {x * y}')
+
+# Divisao
+print(f'{x} / {y} = {x + y}')
+>>>>>>> 587c7ce (implementar a divisao entre dois numeros de forma errada propositalmente (#4))
